9ba324d4d87f2582b4b21c6e1d211cc8beeab179
Topper Bowers <topper@toppingdesign.com>
2019-08-12 22:10:22 +0200

    Export CIDS too

--- src/chaintree/index.ts
+++ src/chaintree/index.ts
@@ -1,2 +1,14 @@
 export * from './chaintree'
 export * from './dag/dag'
+
+import OldCID from 'cids'
+
+/**
+ * This is a helper class so that CID descriptions can be used in other repos.
+ * @public
+ */
+export class CID extends OldCID {
+    constructor(version: string|number|Buffer|CID, codec?: string, multihash?: Buffer, multibaseName?: string) {
+        super(version, codec, multihash, multibaseName)
+    }
+}
