5f5fd5a326f689172fa50c0702ce73213582a755
Topper Bowers <topper@toppingdesign.com>
2019-08-15 21:43:57 +0200

    comments and fix indentation

--- examples/tokens/index.ts
+++ examples/tokens/index.ts
@@ -1,66 +1,68 @@
-import {Repo, getDefault, EcdsaKey, ChainTree, establishTokenTransaction, mintTokenTransaction,  sendTokenTransaction, receiveTokenTransactionFromPayload} from 'tupelo-wasm-sdk';
+import { Repo, getDefault, EcdsaKey, ChainTree, establishTokenTransaction, mintTokenTransaction, sendTokenTransaction, receiveTokenTransactionFromPayload } from 'tupelo-wasm-sdk';
+// use require here because interface-datastore doesn't have any types associated
 const MemoryDatastore: any = require('interface-datastore').MemoryDatastore;
+// also use require for uuidv4 since no types defined.
 const uuidv4: () => string = require('uuid/v4');
 
 // Using an in-memory repo here to avoid having a directory written to the filesystem.
 // getDefault() in the community package will build a default repo for you (on the FS) if you 
-// don't specify a tepo
+// don't specify a repo
 const testRepo = async () => {
-    const repo = new Repo('test', {
-      lock: 'memory',
-      storageBackends: {
-        root: MemoryDatastore,
-        blocks: MemoryDatastore,
-        keys: MemoryDatastore,
-        datastore: MemoryDatastore
-      }
-    })
-    await repo.init({})
-    await repo.open()
-    return repo
-  }
-
-async function executeAsync() {
-    const repo = await testRepo()
-    // connect to the Tupelo testnet (you must be on the internet), but use
-    // a in-memory only IPFS repo.
-    const community = await getDefault(repo)
-
-    // Generate keys for alice and bob
-    console.log("creating keys for alice and bob")
-    const aliceKey = await EcdsaKey.generate()
-    const bobKey = await EcdsaKey.generate()
-
-    // Create empty ChainTrees for alice and bob
-    console.log("creating chaintrees for alice and bob")
-    const aliceTree = await ChainTree.newEmptyTree(community.blockservice, aliceKey)
-    const bobTree = await ChainTree.newEmptyTree(community.blockservice, bobKey)
-
-    const tokenName = "aliceToken"
-    const canonicalName = (await aliceTree.id()) + ":" + tokenName
-
-    console.log("playing the establish token transaction")
-    await community.playTransactions(aliceTree, [establishTokenTransaction(tokenName, 0)]) // 0 means unlimited supply
-    console.log("playing the mint token transaction")
-    await community.playTransactions(aliceTree, [mintTokenTransaction(tokenName, 1)])
-
-    console.log("sending bob a token")
-    const sendId = uuidv4()
-    const payload = await community.sendTokenAndGetPayload(aliceTree, sendTokenTransaction(sendId, canonicalName, 1, (await bobTree.id()) as string))
-
-    console.log("receiving a token on bobs tree")
-    // payload would be sent out-of-band here in a real app
-    await community.playTransactions(bobTree, [receiveTokenTransactionFromPayload(payload)])
-    
-    console.log("success!")
-    community.node.stop()
-    return
-    // YAY! Bob now has one token!
+  const repo = new Repo('test', {
+    lock: 'memory',
+    storageBackends: {
+      root: MemoryDatastore,
+      blocks: MemoryDatastore,
+      keys: MemoryDatastore,
+      datastore: MemoryDatastore
+    }
+  })
+  await repo.init({})
+  await repo.open()
+  return repo
 }
 
-executeAsync().then(()=> {
-    process.exit(0)
+async function executeAsync() {
+  const repo = await testRepo()
+  // connect to the Tupelo testnet (you must be on the internet), but use
+  // a in-memory only IPFS repo.
+  const community = await getDefault(repo)
+
+  // Generate keys for alice and bob
+  console.log("creating keys for alice and bob")
+  const aliceKey = await EcdsaKey.generate()
+  const bobKey = await EcdsaKey.generate()
+
+  // Create empty ChainTrees for alice and bob
+  console.log("creating chaintrees for alice and bob")
+  const aliceTree = await ChainTree.newEmptyTree(community.blockservice, aliceKey)
+  const bobTree = await ChainTree.newEmptyTree(community.blockservice, bobKey)
+
+  const tokenName = "aliceToken"
+  const canonicalName = (await aliceTree.id()) + ":" + tokenName
+
+  console.log("playing the establish token transaction")
+  await community.playTransactions(aliceTree, [establishTokenTransaction(tokenName, 0)]) // 0 means unlimited supply
+  console.log("playing the mint token transaction")
+  await community.playTransactions(aliceTree, [mintTokenTransaction(tokenName, 1)])
+
+  console.log("sending bob a token")
+  const sendId = uuidv4()
+  const payload = await community.sendTokenAndGetPayload(aliceTree, sendTokenTransaction(sendId, canonicalName, 1, (await bobTree.id()) as string))
+
+  console.log("receiving a token on bobs tree")
+  // payload would be sent out-of-band here in a real app
+  await community.playTransactions(bobTree, [receiveTokenTransactionFromPayload(payload)])
+  // YAY! Bob now has one token!
+
+  console.log("success!")
+  community.node.stop()
+  return
+}
+
+executeAsync().then(() => {
+  process.exit(0)
 }, (err) => {
-    console.error(err)
-    process.exit(1)
+  console.error(err)
+  process.exit(1)
 })
