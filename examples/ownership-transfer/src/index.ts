eb4de66c508d4cb01a249b461386e161c37d4719
Arve Knudsen <arve.knudsen@gmail.com>
2019-08-15 16:41:48 +0200

    Add semicolons to example

--- examples/ownership-transfer/src/index.ts
+++ examples/ownership-transfer/src/index.ts
@@ -3,34 +3,34 @@ import assert from 'assert';
 import { ChainTree, EcdsaKey, getDefault, setOwnershipTransaction, Tupelo } from 'tupelo-wasm-sdk';
 
 const main = async () => {
-  const community = await getDefault()
+  const community = await getDefault();
 
   // Create a key pair representing Alice
-  const aliceKey = await EcdsaKey.generate()
+  const aliceKey = await EcdsaKey.generate();
   // Get the address of Alice's key pair
-  const aliceAddress = await Tupelo.ecdsaPubkeyToAddress(aliceKey.publicKey)
+  const aliceAddress = await Tupelo.ecdsaPubkeyToAddress(aliceKey.publicKey);
   // Create a ChainTree representing a trading card, owned by Alice
-  const tradingCard = await ChainTree.newEmptyTree(community.blockservice, aliceKey)
+  const tradingCard = await ChainTree.newEmptyTree(community.blockservice, aliceKey);
 
   // Create a key pair representing Bob
-  const bobKey = await EcdsaKey.generate()
+  const bobKey = await EcdsaKey.generate();
   // Get the address of Bob's key pair
-  const bobAddress = await Tupelo.ecdsaPubkeyToAddress(bobKey.publicKey)
+  const bobAddress = await Tupelo.ecdsaPubkeyToAddress(bobKey.publicKey);
 
   console.log(
     `* Transferring ownership of trading card from Alice to Bob...`
-  )
+  );
   // Transfer ownership of trading card to Bob
   await community.playTransactions(tradingCard, [
     setOwnershipTransaction([bobAddress]),
-  ])
-  await community.nextUpdate()
+  ]);
+  await community.nextUpdate();
 
-  const newOwners = (await tradingCard.resolve(["tree", "_tupelo", 'authentications'])).value
-  assert.deepEqual(newOwners, [bobAddress,])
+  const newOwners = (await tradingCard.resolve(["tree", "_tupelo", 'authentications'])).value;
+  assert.deepEqual(newOwners, [bobAddress,]);
 
-  console.log(`* Transfer successfully made!`)
-}
+  console.log(`* Transfer successfully made!`);
+};
 
 main()
   .then(() => {
@@ -39,4 +39,4 @@ main()
   .catch((err) => {
     console.error(err)
     process.exit(1)
-  })
+  });
