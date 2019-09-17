404d17be52364e1e35beac7edc335aa0bf65224b
Topper Bowers <topper@toppingdesign.com>
2019-09-17 11:04:24 +0200

    make the reading of the actual wasm file lazy so that the path can be somewhere different in a nodejs environment

--- src/js/go/index.js
+++ src/js/go/index.js
@@ -445,7 +445,7 @@ const log = require('debug')('gowasm');
         }
     }
 
-    global.Go.wasmPath = isNodeJS ? global.fs.readFileSync(require("path").join(__dirname, "tupelo.wasm")) : "/tupelo.wasm"
+    global.Go.wasmPath = isNodeJS ? require("path").join(__dirname, "tupelo.wasm") : "/tupelo.wasm"
 
     global.Go.setWasmPath = (path) => {
         global.Go.wasmPath = path;
@@ -467,7 +467,8 @@ const runner = {
         let result
 
         if (isNodeJS) {
-            result = await WebAssembly.instantiate(Go.wasmPath, go.importObject)
+            const wasmBits = global.fs.readFileSync(Go.wasmPath)
+            result = await WebAssembly.instantiate(wasmBits, go.importObject)
         
             process.on("exit", (code) => { // Node.js exits if no event handler is pending
                 Go.exit();
