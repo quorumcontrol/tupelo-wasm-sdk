e873c5128263572bfdff55dea1fad6a5521cab15
Brandon Westcott <brandon@quorumcontrol.com>
2019-11-14 10:41:19 -0500

    github publish workflow fix

--- scripts/ci-release-ver-check.sh
+++ scripts/ci-release-ver-check.sh
@@ -5,7 +5,7 @@ set -e
 release_ver=$(echo "$GITHUB_REF" | rev | cut -d / -f 1 | rev | grep -o -P "\d+\.\d+\.\d+")
 package_ver=$(npm list tupelo-wasm-sdk | grep -o -P "tupelo-wasm-sdk@\d+\.\d+\.\d+" | grep -o -P "\d+\.\d+\.\d+")
 
-if [ "$release_ver" -ne "$package_ver" ]; then
+if [ "$release_ver" != "$package_ver" ]; then
   echo "ERROR: mismatched versions"
   echo " > release version $release_ver"
   echo " > package version $package_ver"
