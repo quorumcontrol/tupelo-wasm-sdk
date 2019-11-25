37371d890dcded68f6852d5c5f7e85761d32e0e1
Topper Bowers <topper@toppingdesign.com>
2019-11-25 17:27:06 +0100

    add a simple in-browser test

---
+++ browsertest/karma.conf.js
@@ -0,0 +1,76 @@
+// Karma configuration
+// Generated on Mon Nov 25 2019 14:20:54 GMT+0100 (Central European Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    frameworks: ["mocha", "karma-typescript"],
+
+    files: [
+        { pattern: "tests/*.ts" },
+        {pattern: 'tupelo.wasm', watched: true, included: false, served: true, nocache: true}
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      '**/*.ts': ['karma-typescript']
+    },
+
+    karmaTypescriptConfig: {
+      compilerOptions: {
+          noImplicitAny: false,
+          target: "ES2015",
+          module: "commonjs",
+          removeComments: true,
+          declaration: true,
+          esModuleInterop: true, 
+          resolveJsonModule:true            
+      }
+  },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ["dots", "karma-typescript"],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
---
+++ browsertest/tests/browser.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+import { Community } from 'tupelo-wasm-sdk'
+
+describe("browser", ()=> {
+    it("hello worlds", ()=> {
+        expect(false).to.be.true
+    })
+
+    // it('communities', ()=> {
+    //     const c = Community.freshLocalTestCommunity()
+    //     expect(c).to.not.be.null
+    // })
+})
