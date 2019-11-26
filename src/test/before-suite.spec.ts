46fe0044eef7728d81f72ad3736ed7c545faf928
Topper Bowers <topper@toppingdesign.com>
2019-11-26 12:15:05 +0100

    fix up the before suite

--- src/test/before-suite.spec.ts
+++ src/test/before-suite.spec.ts
@@ -2,22 +2,46 @@ import isNode from 'detect-node';
 import fs from 'fs';
 import path from 'path';
 import { Community } from '../community';
+import Repo from '../repo';
+const MemoryDatastore: any = require('interface-datastore').MemoryDatastore;
 
-before(async () => {  
-  let tomlConfig: string;
-  if (isNode) {
-    let tomlFile
-    if (fs.existsSync("/tupelo-local/config/notarygroup.toml")) {
-      tomlFile = '/tupelo-local/config/notarygroup.toml'
+// we only want to set the default *once* and not every time, so use a promise
+let beforePromise:Promise<boolean>
+
+before(() => {
+  if (beforePromise !== undefined) {
+    return beforePromise;
+  }  
+  beforePromise = new Promise(async (res,rej)=> {
+    const repo = new Repo('test', {
+      lock: 'memory',
+      storageBackends: {
+        root: MemoryDatastore,
+        blocks: MemoryDatastore,
+        keys: MemoryDatastore,
+        datastore: MemoryDatastore
+      }
+    })
+  await repo.init({})
+  await repo.open()
+
+    let tomlConfig: string;
+    if (isNode) {
+      let tomlFile
+      if (fs.existsSync("/tupelo-local/config/notarygroup.toml")) {
+        tomlFile = '/tupelo-local/config/notarygroup.toml'
+      } else {
+        tomlFile = path.join(__dirname, 'notarygroup.toml')
+      }
+  
+      tomlConfig = fs.readFileSync(tomlFile).toString()
     } else {
-      tomlFile = path.join(__dirname, 'notarygroup.toml')
+      throw new Error("browser not supported yet, you must add notary group resolution over http")
     }
-
-    tomlConfig = fs.readFileSync(tomlFile).toString()
-  } else {
-    throw new Error("browser not supported yet, you must add notary group resolution over http")
-  }
-
-  const testCommunity = await Community.fromNotaryGroupToml(tomlConfig)
-  Community.setDefault(testCommunity)
+  
+    const testCommunity = await Community.fromNotaryGroupToml(tomlConfig, repo)
+    Community.setDefault(testCommunity)
+    res(true)
+  })
+  return beforePromise
 })
