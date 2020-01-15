ad4f3a7f12e4c06a714b69c4229264d8698cacb1
Topper Bowers <topper@toppingdesign.com>
2020-01-15 12:11:24 +0100

    use a beforeSuite

--- src/test/before-suite.spec.ts
+++ src/test/before-suite.spec.ts
@@ -1,3 +1,4 @@
+import Mocha from 'mocha'
 import isNode from 'detect-node';
 import fs from 'fs';
 import path from 'path';
@@ -6,15 +7,13 @@ import Repo from '../repo';
 const MemoryDatastore: any = require('interface-datastore').MemoryDatastore;
 
 // we only want to set the default *once* and not every time, so use a promise
-let beforePromise:Promise<boolean>
+let beforePromise: Promise<boolean>
 
-before(() => {
-  this.timeout(20000)
-
+Mocha.suiteSetup(()=> {
   if (beforePromise !== undefined) {
     return beforePromise;
-  }  
-  beforePromise = new Promise(async (res,rej)=> {
+  }
+  beforePromise = new Promise(async (res, rej) => {
     const repo = new Repo('test', {
       lock: 'memory',
       storageBackends: {
@@ -24,26 +23,55 @@ before(() => {
         datastore: MemoryDatastore
       }
     })
-  await repo.init({})
-  await repo.open()
+    await repo.init({})
+    await repo.open()
 
     let tomlConfig: string;
-    if (isNode) {
-      let tomlFile
-      if (fs.existsSync("/tupelo-local/config/notarygroup.toml")) {
-        tomlFile = '/tupelo-local/config/notarygroup.toml'
-      } else {
-        tomlFile = path.join(__dirname, '../../localtupelo/configs/localdocker.toml')
-      }
-  
-      tomlConfig = fs.readFileSync(tomlFile).toString()
-    } else {
-      throw new Error("browser not supported yet, you must add notary group resolution over http")
+    if (!isNode) {
+      rej(new Error("browser not supported yet"))
+      return
     }
-  
+
+    const tomlFile = path.join(__dirname, '../../localtupelo/configs/localdocker.toml')
+    tomlConfig = fs.readFileSync(tomlFile).toString()
+
     const testCommunity = await Community.fromNotaryGroupToml(tomlConfig, repo)
     Community.setDefault(testCommunity)
     res(true)
   })
   return beforePromise
 })
+
+// before(() => {
+//   if (beforePromise !== undefined) {
+//     return beforePromise;
+//   }
+//   beforePromise = new Promise(async (res, rej) => {
+//     const repo = new Repo('test', {
+//       lock: 'memory',
+//       storageBackends: {
+//         root: MemoryDatastore,
+//         blocks: MemoryDatastore,
+//         keys: MemoryDatastore,
+//         datastore: MemoryDatastore
+//       }
+//     })
+//     await repo.init({})
+//     await repo.open()
+
+//     let tomlConfig: string;
+//     if (!isNode) {
+//       rej(new Error("browser not supported yet"))
+//       return
+//     }
+
+//     const tomlFile = path.join(__dirname, '../../localtupelo/configs/localdocker.toml')
+//     tomlConfig = fs.readFileSync(tomlFile).toString()
+
+
+//     const testCommunity = await Community.fromNotaryGroupToml(tomlConfig, repo)
+//     Community.setDefault(testCommunity)
+//     res(true)
+//   })
+//   return beforePromise
+// })
