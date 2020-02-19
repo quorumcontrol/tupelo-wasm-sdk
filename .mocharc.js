90ceff9408e3dd3adf00c5ffab00d9b5951b4aec
Wes Morgan <wesmorgan@icloud.com>
2020-02-19 09:22:53 -0700

    Increase mocha timeout to 35s

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     exit: true,
     require: 'ts-node/register',
-    timeout: 20000,
+    timeout: 35000, // wait at least the 30s default timeout in tupelo
     spec: ['src/*.spec.ts', 'src/**/*.spec.ts'],
 }
